4f13133bd0ff3bdc872741621358921e467d73bc
jasmin <chjasmin27@gmail.com>
2025-07-11 16:52:16 +0600

    loading component fix

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -10,7 +10,7 @@ const Loading: React.FC<LoadingProps> = ({ loading }) => {
   if (!loading) return null;
 
   return (
-    <div className="max-w-7xl mx-auto p-4 h-screen flex justify-center items-center">
+    <div className="max-w-7xl mx-auto h-screen flex justify-center items-center">
       <div className="text-center">
         {/* Light mode spinner */}
         <div className="block dark:hidden">
